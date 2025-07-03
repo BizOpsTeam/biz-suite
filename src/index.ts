fa5686b87adb8aa456f71444a1d768b455b42ff7
cliff-codes <simplecodes2580@gmail.com>
2025-07-03 12:38:30 +0000

    feat: add global error handling for uncaught exceptions and unhandled rejections in the backend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import app from "./app"
 import dotenv from "dotenv"
 
+process.on('uncaughtException', err => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', err => {
+  console.error('Unhandled Rejection:', err);
+  process.exit(1);
+});
+console.log('Starting Biz-Suite backend...');
+
+
 //load Env variables
 dotenv.config()
 
