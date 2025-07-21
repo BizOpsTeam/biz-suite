5ce0211c61bf9d40ed472a6d993ca791855bf257
cliff-codes <simplecodes2580@gmail.com>
2025-07-21 16:51:31 +0000

    fix: restrict CORS configuration to specific origin for security
    - Updated CORS settings to allow requests only from http://localhost:5173, enhancing security by limiting access to trusted sources.
    - Maintained support for various HTTP methods and headers to ensure continued API functionality.

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ setupSwagger(app);
 //----------------cors config-----------------------//
 app.use(
     cors({
-        origin: "*",
+        origin: ["http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization", "*"],
         credentials: true,
